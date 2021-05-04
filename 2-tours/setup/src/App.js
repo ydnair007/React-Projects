c1ccc2a2603b462a4572f786379f7fe056692416
ydnair007 <pnrajeshnair@gmail.com>
2021-05-04 13:00:02 +0530

    First Push

---
+++ 2-tours/setup/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const rtours = await response.json();
+      setLoading(false);
+      setTours(rtours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        {" "}
+        <Loading></Loading>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours}></Tours>
+    </main>
+  );
+}
+
+export default App;
